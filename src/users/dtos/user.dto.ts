107312800dfffaff68a80b491682256efb62c472
Eggyf <abdelfregel1711@gmail.com>
2024-06-29 16:33:22 -0400

    feat: add swagger

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,11 +1,13 @@
+import { IsEmail, IsNumber, IsString } from "class-validator";
+import { PartialType } from "@nestjs/swagger";
 export class CreateUserDto {
+    @IsString()
+
     readonly name: string;
+    @IsNumber()
     readonly edad: number;
+    @IsEmail()
     readonly email: string;
 }
 
-export class UpdateUserDto {
-    readonly name?: string;
-    readonly edad?: number;
-    readonly email?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) { }
